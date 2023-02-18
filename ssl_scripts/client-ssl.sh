1dcd3bd3151afb3f89f686cca56f365c8f278968
glebka100500 <55336495+glebka100500@users.noreply.github.com>
2023-02-18 00:07:15 -0500

    Update client-ssl.sh
    sort out directories

--- ssl_scripts/client-ssl.sh
+++ ssl_scripts/client-ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 openssl req -newkey rsa:4096 -keyout client-key.pem -nodes -out client.csr -nodes
-openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client.pem
+openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out ../certificates/client.pem
+rm client.csr
