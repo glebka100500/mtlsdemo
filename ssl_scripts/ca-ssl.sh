a1dc149ba5773826875312384b92e9d028552805
glebka100500 <55336495+glebka100500@users.noreply.github.com>
2023-02-18 00:06:22 -0500

    Update ca-ssl.sh
    set directories for keys and certificates

--- ssl_scripts/ca-ssl.sh
+++ ssl_scripts/ca-ssl.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-openssl req -newkey rsa:4096 -x509 -keyout ca-key.pem -out ca-cert.pem -nodes
+openssl req -newkey rsa:4096 -x509 -keyout keys/ca-key.pem -out certificates/ca-cert.pem -nodes
