bc61933786017a015de3565e215e35f6d394c61e
Gleb Korniienko <glebkorniienko@Glebs-MacBook-Pro.local>
2023-02-18 00:21:35 -0500

    organize

--- ssl_scripts/server-ssl.sh
+++ ssl_scripts/server-ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-openssl req -newkey rsa:4096 -keyout server-key.pem -out server.csr -nodes
-openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.pem
+openssl req -newkey rsa:4096 -keyout ../keys/server-key.pem -out server.csr -nodes
+openssl x509 -req -in server.csr -CA ../certificates/ca-cert.pem -CAkey ../keys/ca-key.pem -CAcreateserial -out ../certificates/server.pem
+rm server.csr
